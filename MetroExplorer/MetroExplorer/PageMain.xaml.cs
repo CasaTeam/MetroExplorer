8b805559e35c3bb686c8cde16c67be477bcad737
comiscience <comiscience@gmail.com>
2013-03-08 23:47:14 +0100

    2013 03 08
    首页功能丰富

--- MetroExplorer/MetroExplorer/PageMain.xaml.cs
+++ MetroExplorer/MetroExplorer/PageMain.xaml.cs
@@ -18,6 +18,7 @@ using System.ComponentModel;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using WinRTXamlToolkit.Controls.Extensions;
+using Windows.Storage.AccessCache;
 
 // Pour en savoir plus sur le modèle d'élément Page Éléments groupés, consultez la page http://go.microsoft.com/fwlink/?LinkId=234231
 
@@ -28,8 +29,8 @@ namespace MetroExplorer
     /// </summary>
     public sealed partial class PageMain : LayoutAwarePage, INotifyPropertyChanged
     {
-        private ObservableCollection<GroupInfoList<ExplorerItem>> _explorerItems;
-        public ObservableCollection<GroupInfoList<ExplorerItem>> ExplorerItems
+        private ObservableCollection<ExplorerItem> _explorerItems;
+        public ObservableCollection<ExplorerItem> ExplorerItems
         {
             get { return _explorerItems; }
             set
@@ -43,51 +44,97 @@ namespace MetroExplorer
         {
             this.InitializeComponent();
             DataContext = this;
-            ExplorerItems = new ObservableCollection<GroupInfoList<ExplorerItem>>();
+            ExplorerItems = new ObservableCollection<ExplorerItem>();
 
             this.Loaded += PageMain_Loaded;
         }
 
-        void PageMain_Loaded(object sender, RoutedEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            GroupInfoList<ExplorerItem> groupInfoList = new GroupInfoList<ExplorerItem>();
-            
-            groupInfoList.Key = "系统磁盘及文件库";
-            for (int i = 0; i < 50; i++)
-            {
-                groupInfoList.Add(new ExplorerItem()
-                {
-                    Name = KnownFolders.PicturesLibrary.Name,
-                    Path = KnownFolders.PicturesLibrary.Path,
-                    StorageFolder = KnownFolders.PicturesLibrary,
-                    Type = ExplorerItemType.Folder
-                });
-                groupInfoList.Add(new ExplorerItem()
-                {
-                    Name = KnownFolders.MusicLibrary.Name,
-                    Path = KnownFolders.MusicLibrary.Path,
-                    StorageFolder = KnownFolders.MusicLibrary,
-                    Type = ExplorerItemType.Folder
-                });
-                groupInfoList.Add(new ExplorerItem()
-                {
-                    Name = KnownFolders.DocumentsLibrary.Name,
-                    Path = KnownFolders.DocumentsLibrary.Path,
-                    StorageFolder = KnownFolders.DocumentsLibrary,
-                    Type = ExplorerItemType.Folder
-                });
-                groupInfoList.Add(new ExplorerItem()
-                {
-                    Name = KnownFolders.VideosLibrary.Name,
-                    Path = KnownFolders.VideosLibrary.Path,
-                    StorageFolder = KnownFolders.VideosLibrary,
-                    Type = ExplorerItemType.Folder
-                });
-            }
-            ExplorerItems.Add(groupInfoList);
+            this.AppBar_BottomAppBar.IsOpen = true;
+        }
+
+        async void PageMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            initializeSystemFolders();
+            await initializeUsersFolders();
 
             ScrollViewer myScrollViewer = itemGridView.GetFirstDescendantOfType<ScrollViewer>();
             myScrollViewer.ViewChanged += myScrollViewer_ViewChanged;
+        }
+
+        private async System.Threading.Tasks.Task initializeUsersFolders()
+        {
+            if (Windows.Storage.AccessCache.StorageApplicationPermissions.MostRecentlyUsedList != null && Windows.Storage.AccessCache.StorageApplicationPermissions.MostRecentlyUsedList.Entries.Count > 0)
+            {
+                foreach (var item in Windows.Storage.AccessCache.StorageApplicationPermissions.MostRecentlyUsedList.Entries)
+                {
+                    var retrievedItem = await StorageApplicationPermissions.MostRecentlyUsedList.GetItemAsync(item.Token);
+                    if (retrievedItem is StorageFolder)
+                    {
+                        StorageFolder retrievedFolder = retrievedItem as StorageFolder;
+                        ExplorerItems.Add(new ExplorerItem()
+                        {
+                            Name = retrievedFolder.Name,
+                            Path = retrievedFolder.Path,
+                            StorageFolder = retrievedFolder,
+                            Type = ExplorerItemType.Folder
+                        });
+                    }
+                }
+            }
+        }
+
+        private void initializeSystemFolders()
+        {
+            ExplorerItems.Add(new ExplorerItem()
+            {
+                Name = KnownFolders.PicturesLibrary.Name,
+                Path = KnownFolders.PicturesLibrary.Path,
+                StorageFolder = KnownFolders.PicturesLibrary,
+                Type = ExplorerItemType.Folder
+            });
+            ExplorerItems.Add(new ExplorerItem()
+            {
+                Name = KnownFolders.MusicLibrary.Name,
+                Path = KnownFolders.MusicLibrary.Path,
+                StorageFolder = KnownFolders.MusicLibrary,
+                Type = ExplorerItemType.Folder
+            });
+            ExplorerItems.Add(new ExplorerItem()
+            {
+                Name = KnownFolders.DocumentsLibrary.Name,
+                Path = KnownFolders.DocumentsLibrary.Path,
+                StorageFolder = KnownFolders.DocumentsLibrary,
+                Type = ExplorerItemType.Folder
+            });
+            ExplorerItems.Add(new ExplorerItem()
+            {
+                Name = KnownFolders.VideosLibrary.Name,
+                Path = KnownFolders.VideosLibrary.Path,
+                StorageFolder = KnownFolders.VideosLibrary,
+                Type = ExplorerItemType.Folder
+            });
+            if (KnownFolders.RemovableDevices != null)
+            {
+                ExplorerItems.Add(new ExplorerItem()
+                {
+                    Name = KnownFolders.RemovableDevices.Name,
+                    Path = KnownFolders.RemovableDevices.Path,
+                    StorageFolder = KnownFolders.RemovableDevices,
+                    Type = ExplorerItemType.Folder
+                });
+            }
+            if (KnownFolders.MediaServerDevices != null)
+            {
+                ExplorerItems.Add(new ExplorerItem()
+                {
+                    Name = KnownFolders.MediaServerDevices.Name,
+                    Path = KnownFolders.MediaServerDevices.Path,
+                    StorageFolder = KnownFolders.MediaServerDevices,
+                    Type = ExplorerItemType.Folder
+                });
+            }
         }
 
         void myScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
@@ -115,6 +162,50 @@ namespace MetroExplorer
             // TODO: assignez une collection de groupes pouvant être liés à this.DefaultViewModel["Groups"]
         }
 
+        private void itemGridView_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ExplorerItem item = itemGridView.SelectedItem as ExplorerItem;
+
+                itemGridView.SelectedItem = null;
+
+        }
+
+        private async System.Threading.Tasks.Task addNewFolder()
+        {
+            FolderPicker folderPicker = new FolderPicker();
+            folderPicker.ViewMode = PickerViewMode.List;
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add("*");
+            StorageFolder storageFolder = await folderPicker.PickSingleFolderAsync();
+            if (storageFolder != null)
+            {
+                ExplorerItem eplItem = new ExplorerItem()
+                {
+                    Name = storageFolder.Name,
+                    Path = storageFolder.Path,
+                    StorageFolder = storageFolder,
+                    Type = ExplorerItemType.Folder
+                };
+                ExplorerItems.Add(eplItem);
+                Windows.Storage.AccessCache.StorageApplicationPermissions.MostRecentlyUsedList.Add(storageFolder, storageFolder.Name);
+            }
+        }
+
+        private async void Button_AddNewDiskFolder_Click(object sender, RoutedEventArgs e)
+        {
+            await addNewFolder();
+        }
+
+        private void Button_RemoveDiskFolder_Click(object sender, RoutedEventArgs e)
+        {
+            if (itemGridView.SelectedItems == null || itemGridView.SelectedItems.Count == 0) return;
+            for (int i = itemGridView.SelectedItems.Count - 1; i > -1 ; i--)
+            {
+                ExplorerItems.Remove(itemGridView.SelectedItems[i] as ExplorerItem);
+            }
+        }
+
+        #region propertychanged
         private void NotifyPropertyChanged(String changedPropertyName)
         {
             if (PropertyChanged != null)
@@ -124,6 +215,12 @@ namespace MetroExplorer
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        private void itemGridView_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            BottomAppBar.IsOpen = true;
+        }
     }
 
     public class GroupInfoList<T> : ObservableCollection<T>
