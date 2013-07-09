f3f6145e6c6ca5d00a24b6be1c08c418d110431e
Sawyer LIN <sawyer.lin@gmail.com>
2013-07-09 23:47:02 +0200

    Add Map Control into the solution

---
+++ MetroExplorer/MetroExplorer.Components.Maps/MapList.cs
@@ -0,0 +1,20 @@
+﻿namespace MetroExplorer.Components.Maps
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Documents;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+
+    public sealed class MapList : Control
+    {
+        public MapList()
+        {
+            this.DefaultStyleKey = typeof(MapList);
+        }
+    }
+}
